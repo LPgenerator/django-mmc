7342610da29a89bdc51f33986cb4ce920eb93b60
gotlium <gotlium@gmail.com>
2014-08-05 01:09:48 +0500

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-mmc',
-    version="1.1.1",
+    version="1.2",
     description='Monitor management commands on Django',
     keywords="django management",
     long_description=open('README.rst').read(),
