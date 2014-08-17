ba6d65d941a448c673691a214bcb3d6e4854a636
gotlium <gotlium@gmail.com>
2014-08-17 08:25:31 +0500

    internal vars was renamed

--- mmc/mixins.py
+++ mmc/mixins.py
@@ -24,24 +24,24 @@ class BaseCommandMixin(object):
     def __init__(self):
         self._no_monkey.__init__(self)
 
-        self._start_date = now()
-        self._start_time = time.time()
-        self._elapsed = None
-        self._success = True
-        self._error_message = None
-        self._traceback = None
-        self._show_traceback = False
-        self._script = self.__module__.split('.')[-1]
-        self._lock = get_lock_instance(self._script)
+        self._mmc_start_date = now()
+        self._mmc_start_time = time.time()
+        self._mmc_elapsed = None
+        self._mmc_success = True
+        self._mmc_error_message = None
+        self._mmc_traceback = None
+        self._mmc_show_traceback = False
+        self._mmc_script = self.__module__.split('.')[-1]
+        self._mmc_lock = get_lock_instance(self._mmc_script)
 
     def __mmc_one_copy(self):
         try:
             from models import MMCScript
 
             try:
-                if MMCScript.get_one_copy(self._script):
-                    self._lock.check_is_running()
-                    self._lock.lock()
+                if MMCScript.get_one_copy(self._mmc_script):
+                    self._mmc_lock.check_is_running()
+                    self._mmc_lock.lock()
             except MMCScript.DoesNotExist:
                 pass
         except Exception, msg:
@@ -50,14 +50,14 @@ class BaseCommandMixin(object):
     def execute(self, *args, **options):
         self.__mmc_one_copy()
         atexit.register(self._mmc_atexit_callback)
-        self._show_traceback = options.get('traceback', False)
+        self._mmc_show_traceback = options.get('traceback', False)
 
         try:
             self._no_monkey.execute(self, *args, **options)
         except Exception as ex:
-            self._success = False
-            self._error_message = ex.__unicode__()
-            self._traceback = traceback.format_exc()
+            self._mmc_success = False
+            self._mmc_error_message = ex.__unicode__()
+            self._mmc_traceback = traceback.format_exc()
             raise
 
     def __mmc_store_log(self):
@@ -65,38 +65,38 @@ class BaseCommandMixin(object):
             from mmc.models import MMCLog
 
             MMCLog.logging(
-                start=self._start_date,
+                start=self._mmc_start_date,
                 hostname=socket.gethostname(),
-                script=self._script,
-                elapsed="%0.2f" % (time.time() - self._start_time),
-                success=self._success,
-                error_message=self._error_message,
-                traceback=self._traceback,
+                script=self._mmc_script,
+                elapsed="%0.2f" % (time.time() - self._mmc_start_time),
+                success=self._mmc_success,
+                error_message=self._mmc_error_message,
+                traceback=self._mmc_traceback,
                 sys_argv=' '.join(map(unicode, sys.argv))
             )
         except Exception, msg:
             print '[MMC] Logging broken with message:', msg.__unicode__()
 
     def __mmc_send_mail(self):
-        if not self._success:
+        if not self._mmc_success:
             from mmc.models import MMCEmail
 
-            MMCEmail.send(self._traceback)
+            MMCEmail.send(self._mmc_traceback)
 
     def __mmc_print_log(self):
-        if not self._success:
-            if self._show_traceback:
-                print self._traceback
+        if not self._mmc_success:
+            if self._mmc_show_traceback:
+                print self._mmc_traceback
             else:
                 sys.stderr.write(smart_str(
-                    'Error: %s\n' % self.style.ERROR(self._error_message)
+                    'Error: %s\n' % self.style.ERROR(self._mmc_error_message)
                 ))
             sys.exit(1)
 
     def _mmc_atexit_callback(self, *args, **kwargs):
         self.__mmc_store_log()
         self.__mmc_send_mail()
-        self._lock.unlock()
+        self._mmc_lock.unlock()
         self.__mmc_print_log()
 
 
