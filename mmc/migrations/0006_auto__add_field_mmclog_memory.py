19db143b8ff13d65024ada019aca4f7f83567bc5
gotlium <gotlium@gmail.com>
2014-08-17 09:23:28 +0500

    memory usage migrations

---
+++ mmc/migrations/0006_auto__add_field_mmclog_memory.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'MMCLog.memory'
+        db.add_column(u'mmc_mmclog', 'memory',
+                      self.gf('django.db.models.fields.FloatField')(default=0.0),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'MMCLog.memory'
+        db.delete_column(u'mmc_mmclog', 'memory')
+
+
+    models = {
+        u'mmc.mmcemail': {
+            'Meta': {'object_name': 'MMCEmail'},
+            'created': ('django.db.models.fields.DateField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
+        },
+        u'mmc.mmchost': {
+            'Meta': {'object_name': 'MMCHost'},
+            'created': ('django.db.models.fields.DateField', [], {'auto_now': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ignore': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        u'mmc.mmclog': {
+            'Meta': {'object_name': 'MMCLog'},
+            'created': ('django.db.models.fields.DateField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'elapsed': ('django.db.models.fields.FloatField', [], {}),
+            'end': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'error_message': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'hostname': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['mmc.MMCHost']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'memory': ('django.db.models.fields.FloatField', [], {'default': '0.0'}),
+            'script': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['mmc.MMCScript']"}),
+            'start': ('django.db.models.fields.DateTimeField', [], {}),
+            'success': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'sys_argv': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'traceback': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'mmc.mmcscript': {
+            'Meta': {'object_name': 'MMCScript'},
+            'created': ('django.db.models.fields.DateField', [], {'auto_now': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ignore': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'one_copy': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'save_on_error': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        }
+    }
+
+    complete_apps = ['mmc']
