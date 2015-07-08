bd03dd88e5ec85c43446de5427726d207cfef3b0
gotlium <gotlium@gmail.com>
2015-07-09 02:21:41 +0300

    add vagrant bootstrap file

---
+++ demo/.vagrant_bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y git python python-dev python-pip
+apt-get clean
+
+cd /mmc_demo/
+pip install -r requirements.txt
+
+python manage.py syncdb --noinput
+python manage.py migrate --noinput
+python manage.py loaddata auth.json
+
+python manage.py test_command >& /dev/null &
+python manage.py test_command_noargs >& /dev/null &
+python manage.py test_command_error >& /dev/null &
+python manage.py test_command_killed >& /dev/null &
+
+python manage.py runserver 0.0.0.0:8000 >& /dev/null &
